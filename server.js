ca7c4b27f41a054931868f4f6005f676e8d68a31
Roshan Adhikari <nahsorad@gmail.com>
2019-03-15 11:54:48 +0545

    Prevent duplicate tweets in any interval

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 3000;
 const URL = 'https://quotes.rest/qod';
  
 const T = new Twit({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.BOT_CONSUMER_KEY || 'NCVQwjaDzDRyXuMNp0tKLxKN0',
+    consumer_secret: process.env.BOT_CONSUMER_SECRET || 'DbBNaadqqoxNINjg4L3tk3P0WPSKxae8QJlZXgpOkM9WhO6Rf1',
+    access_token: process.env.BOT_ACCESS_TOKEN || '1103973630520655872-Pd0k2EUxSTLxevViC6zKPyADuJrQre',
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET || 'lrfTpwdSN5VFu6AW04GrE201baY8qsiTu05uPhqsEzsed',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
@@ -16,7 +16,7 @@ const T = new Twit({
 function tweetIt(){
     let tweetMsg = '';
     let hashTags = ' #DailyQuotes #QuoteOfTheDay'
-    var lenQuote, lenAuthor, lenHashTags, lenTweetMsg;
+    var lenQuote, lenAuthor, lenHashTags, lenTweetMsg, isAlreadyTweeted;
     request.get(URL, (err, res, body) => {
         if(err) console.log(err);
 
@@ -37,10 +37,21 @@ function tweetIt(){
 
         lenTweetMsg = tweetMsg.length;
         console.log(tweetMsg, lenTweetMsg);
-        
-        T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
-            if(err) console.log(err, tweetMsg);
-        });
+
+        // Search if the tweet has already been tweeted by the accound
+        T.get('search/tweets', { q: '"' + tweetMsg +'" from:daytheofquote', count: 100 }, function(err, data, response) {
+            if(data.statuses.length == 0){
+                // Tweet does not exist as of yet
+                console.log("Tweet IS NOT a duplicate!");
+                T.post('statuses/update', { status: tweetMsg }, function(err, data, response) {
+                    if(err) console.log(err, tweetMsg);
+                });
+            }
+            else{
+                // Tweet already exists
+                console.log("Tweet IS a duplicate!");
+            }
+          });
     });
 }
 const app = express();
